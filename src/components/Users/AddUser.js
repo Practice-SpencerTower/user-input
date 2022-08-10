fff7f7bdc7f2bf9a1dc5cc4fc265d004a50259cf
SpencerTower <spencerjtower1@gmail.com>
2022-08-10 14:40:32 -0700

    adds input field css styles

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from '../UI/Card';
+import './AddUser.module.css';
 
 const AddUser = props => {
     const addUserHandler = (e) => {
@@ -10,9 +11,9 @@ const AddUser = props => {
     <Card>
     <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"/>
+        <input id="username" type="text" className={classes.input}/>
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number"/>
+        <input id="age" type="number" className={classes.input}/>
         <button type="submit">Add User</button>
     </form>
     </Card>
