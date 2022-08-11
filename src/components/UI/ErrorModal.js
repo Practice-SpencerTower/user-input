65e68604fa3af0cfa084082d5d824333bb9c281b
SpencerTower <spencerjtower1@gmail.com>
2022-08-11 12:56:39 -0700

    adds react portal to ErrorModal to render Backdrop and ModalOverlay

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,30 +1,47 @@
-import React from "react";
-import Card from "./Card";
-import Button from "./Button";
+import React from 'react';
+import Card from './Card';
+import Button from './Button';
 import classes from './ErrorModal.module.css';
+import ReactDOM from 'react-dom';
 
-const ErrorModal = props => {
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
     return (
-        <>
-        <div className={classes.backdrop} onClick={props.onConfirm}>
-        </div>
         <Card className={classes.modal}>
             <header className={classes.header}>
-                <h2>
-                    {props.title}
-                </h2>
+                <h2>{props.title}</h2>
             </header>
             <div className={classes.content}>
-                <p>
-                    {props.message}
-                </p>
+                <p>{props.message}</p>
             </div>
             <footer className={classes.actions}>
                 <Button onClick={props.onConfirm}>Okay</Button>
             </footer>
         </Card>
+    );
+};
+
+const ErrorModal = (props) => {
+    return (
+        <>
+            {ReactDOM.createPortal(
+                <Backdrop onConfirm={props.onConfirm} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onConfirm={props.onConfirm}
+                />,
+                // tell where to render the ModalOverlay
+                document.getElementById('overlay-root')
+            )}
         </>
-    )
+    );
 };
 
 export default ErrorModal;
